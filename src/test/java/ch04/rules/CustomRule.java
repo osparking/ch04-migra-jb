a4571adcd02bae83475d5f234b589ccc1e10e43e
osparking <jbpark03@gmail.com>
2023-11-28 20:47:39 +0900

    맞춤 규칙 - 추가 행동 포함된 문장 사용

---
+++ src/test/java/ch04/rules/CustomRule.java
@@ -0,0 +1,17 @@
+package ch04.rules;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class CustomRule implements TestRule {
+	private Statement base;
+	private Description description;
+
+	@Override
+	public Statement apply(Statement base, Description description) {
+		this.base = base;
+		this.description = description;
+		return new CustomStatement(base, description);
+	}
+}
